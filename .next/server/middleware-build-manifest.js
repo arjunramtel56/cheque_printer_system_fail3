514579631267f93d931f3ddd7f68e3f37c0c457a
Arjun Ramtel <arjunramtel56@gmail.com>
2026-09-15 12:06:20 +0545

    feat: add build, client middleware, and SSG manifests for improved static generation and middleware handling

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -7,9 +7,9 @@ globalThis.__BUILD_MANIFEST = {
     "static/chunks/0cz1d0mv5g_q7.js"
   ],
   "lowPriorityFiles": [
-    "static/27-UiuyDU1UIk8b2MPft5/_buildManifest.js",
-    "static/27-UiuyDU1UIk8b2MPft5/_ssgManifest.js",
-    "static/27-UiuyDU1UIk8b2MPft5/_clientMiddlewareManifest.js"
+    "static/l6De3Jkm7WLFJ6GE6xGKp/_buildManifest.js",
+    "static/l6De3Jkm7WLFJ6GE6xGKp/_ssgManifest.js",
+    "static/l6De3Jkm7WLFJ6GE6xGKp/_clientMiddlewareManifest.js"
   ],
   "rootMainFiles": [
     "static/chunks/3l04zcqx63h3y.js",
