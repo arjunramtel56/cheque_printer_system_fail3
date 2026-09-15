7398e5333ebac7e4562dc1d4b72dc90153879d3c
Arjun Ramtel <arjunramtel56@gmail.com>
2026-09-15 12:07:44 +0545

    Add build, client middleware, and SSG manifests for static generation
    - Created _buildManifest.js to manage rewrites and sorted pages.
    - Added _clientMiddlewareManifest.js to handle middleware matchers.
    - Introduced _ssgManifest.js to define the SSG manifest as an empty set.

--- .next/server/app/index.segments/_tree.segment.rsc
+++ .next/server/app/index.segments/_tree.segment.rsc
@@ -1,2 +1,2 @@
 :HL["/_next/static/chunks/0h1ualhxfm7eu.css","style"]
-0:{"tree":{"name":"","param":null,"prefetchHints":4176,"slots":{"children":{"name":"__PAGE__","param":null,"prefetchHints":4256,"slots":null}}},"staleTime":300,"buildId":"l6De3Jkm7WLFJ6GE6xGKp"}
+0:{"tree":{"name":"","param":null,"prefetchHints":4176,"slots":{"children":{"name":"__PAGE__","param":null,"prefetchHints":4256,"slots":null}}},"staleTime":300,"buildId":"hV98Uq-n8xShspDXmXZDt"}
--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -7,9 +7,9 @@ globalThis.__BUILD_MANIFEST = {
     "static/chunks/0cz1d0mv5g_q7.js"
   ],
   "lowPriorityFiles": [
-    "static/l6De3Jkm7WLFJ6GE6xGKp/_buildManifest.js",
-    "static/l6De3Jkm7WLFJ6GE6xGKp/_ssgManifest.js",
-    "static/l6De3Jkm7WLFJ6GE6xGKp/_clientMiddlewareManifest.js"
+    "static/hV98Uq-n8xShspDXmXZDt/_buildManifest.js",
+    "static/hV98Uq-n8xShspDXmXZDt/_ssgManifest.js",
+    "static/hV98Uq-n8xShspDXmXZDt/_clientMiddlewareManifest.js"
   ],
   "rootMainFiles": [
     "static/chunks/3l04zcqx63h3y.js",
